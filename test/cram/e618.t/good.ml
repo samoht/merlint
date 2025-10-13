2bf6e474caa298a22166d17a73cbe82cf908e752
Thomas Gazagnaire <thomas@gazagnaire.org>
2025-10-13 16:04:36 +0200

    E618 rule no longer flags OCaml special identifiers like __LOC__ and __FILE_

--- test/cram/e618.t/good.ml
+++ test/cram/e618.t/good.ml
@@ -1,2 +1,9 @@
 let example1 = Printf.sprintf "hello"
 let example2 = String.length "test"
+
+(* OCaml special identifiers should not trigger E618 *)
+let example3 = __LOC__
+let example4 = __FILE__
+let example5 = __LINE__
+let example6 = __MODULE__
+let example7 = __POS__
