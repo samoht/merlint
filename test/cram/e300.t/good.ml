40b2e114731504b5621ec445f31a1bf910609e70
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-03-10 13:23:13 -0700

    chore(merlint): update style guide docs and test fixtures

--- test/cram/e300.t/good.ml
+++ test/cram/e300.t/good.ml
@@ -1,4 +1,9 @@
-type status = 
+type status =
   | Waiting_for_input  (* Snake_case *)
   | Processing_data
   | Error_occurred
+
+type os =
+  | Linux
+  | MacOS       (* short trailing acronym: compound term *)
+  | Windows
