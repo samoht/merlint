a8c3f624e8304ef38061155b74d86fedcd69a9b3
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-04-13 13:09:36 -0700

    irmin: clean up dead files, document structure

--- lib/rules/e900.ml
+++ lib/rules/e900.ml
@@ -2,42 +2,49 @@
 
 type payload = { package : string }
 
+(** Walk <pkg>/lib/*.ml looking for Wire.Codec or Wire.Field usage. If found and
+    <pkg>/c/ doesn't exist, flag it. *)
 let check (ctx : Context.project) =
-  let desc = Context.dune_describe ctx in
-  let libs = Dune.libraries desc in
+  let root = ctx.project_root in
+  let try_readdir d =
+    try Sys.readdir d |> Array.to_list with Sys_error _ -> []
+  in
   let issues = ref [] in
+  let packages = try_readdir root in
   List.iter
-    (fun (lib : Dune.library_info) ->
-      let has_wire =
-        List.exists
-          (fun f ->
-            Fpath.has_ext ".ml" f
-            &&
-              try
-                let content =
-                  In_channel.with_open_text (Fpath.to_string f)
-                    In_channel.input_all
-                in
-                Astring.String.is_infix ~affix:"Wire.Codec" content
-                || Astring.String.is_infix ~affix:"Wire.Field" content
-              with _ -> false)
-          lib.files
-      in
-      if has_wire then
-        let lib_dir =
-          match lib.files with f :: _ -> Fpath.parent f | [] -> Fpath.v "."
-        in
-        let pkg_dir =
-          if Fpath.basename lib_dir = "lib" then Fpath.parent lib_dir
-          else lib_dir
-        in
-        let c_dir = Fpath.(pkg_dir / "c") in
-        if
-          not
-            (Sys.file_exists (Fpath.to_string c_dir)
-            && Sys.is_directory (Fpath.to_string c_dir))
-        then issues := Issue.v { package = Fpath.to_string pkg_dir } :: !issues)
-    libs;
+    (fun pkg ->
+      let pkg_dir = Filename.concat root pkg in
+      if
+        Sys.file_exists pkg_dir && Sys.is_directory pkg_dir && pkg <> "_build"
+        && pkg <> ".git" && pkg <> "_opam"
+      then
+        let lib_dir = Filename.concat pkg_dir "lib" in
+        if Sys.file_exists lib_dir && Sys.is_directory lib_dir then
+          let ml_files =
+            try_readdir lib_dir
+            |> List.filter (fun f ->
+                Filename.check_suffix f ".ml"
+                && not (Filename.check_suffix f ".mli"))
+          in
+          let has_wire =
+            List.exists
+              (fun f ->
+                try
+                  let content =
+                    In_channel.with_open_text
+                      (Filename.concat lib_dir f)
+                      In_channel.input_all
+                  in
+                  Astring.String.is_infix ~affix:"Wire.Codec" content
+                  || Astring.String.is_infix ~affix:"Wire.Field" content
+                with _ -> false)
+              ml_files
+          in
+          if has_wire then
+            let c_dir = Filename.concat pkg_dir "c" in
+            if not (Sys.file_exists c_dir && Sys.is_directory c_dir) then
+              issues := Issue.v { package = pkg } :: !issues)
+    packages;
   !issues
 
 let pp ppf { package } =
