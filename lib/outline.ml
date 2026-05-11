9e9cd881887526f1cee88c52ddec655fbb3414c7
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-05-11 14:40:31 -0700

    merlint: track Merlin.Outline namespace promotion
    ocaml-merlin no longer flattens the Outline helpers at the top level; the
    in-tree Outline module now lives at Merlin.Outline. Update merlint's thin
    re-export wrapper accordingly. Test files inline their own parse-core-type
    helper since it's no longer in the public API.

--- lib/outline.ml
+++ lib/outline.ml
@@ -1,6 +1,6 @@
 (** OCamlmerlin outline output - thin re-export over [Merlin.outline]. *)
 
-type kind = Merlin.symbol_kind =
+type kind = Merlin.Outline.symbol_kind =
   | Value
   | Type
   | Module
@@ -13,18 +13,18 @@ type kind = Merlin.symbol_kind =
   | Method
   | Label
 
-type item = Merlin.outline_item
-type t = Merlin.outline
+type item = Merlin.Outline.item
+type t = Merlin.Outline.t
 
-let flatten = Merlin.flatten_outline
-let values = Merlin.values
-let by_name = Merlin.by_name
-let parsed_type = Merlin.parsed_type
-let is_function_type = Merlin.is_function_type
-let return_type = Merlin.return_type
-let count_parameters = Merlin.count_parameters
-let returns_option = Merlin.returns_option
-let pp = Merlin.pp_outline
+let flatten = Merlin.Outline.flatten
+let values = Merlin.Outline.values
+let by_name = Merlin.Outline.by_name
+let parsed_type = Merlin.Outline.parsed_type
+let is_function_type = Merlin.Outline.is_function_type
+let return_type = Merlin.Outline.return_type
+let count_parameters = Merlin.Outline.count_parameters
+let returns_option = Merlin.Outline.returns_option
+let pp = Merlin.Outline.pp
 
 let location filename (item : item) =
   let loc = item.location in
