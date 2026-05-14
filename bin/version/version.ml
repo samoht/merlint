a87ed5e6a14ba3d1b1dc065eb336bd2db88697ff
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-05-14 16:31:12 -0700

    Add Bottler project version scaffolding

--- bin/version/version.ml
+++ bin/version/version.ml
@@ -1,4 +1,7 @@
 let version =
-  match Build_info.V1.version () with
-  | Some v -> Build_info.V1.Version.to_string v
-  | None -> Git_hash.hash
+  match Git_hash.hash with
+  | "dev" -> (
+      match Build_info.V1.version () with
+      | Some v -> Build_info.V1.Version.to_string v
+      | None -> "dev")
+  | v -> v
