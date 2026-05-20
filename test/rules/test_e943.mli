eb2d17f8ab40c2c0db8a49f6704953d437aee1c9
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-05-19 21:58:27 -0700

    merlint/test + monopam/config: lead docs with the value name
    E943/E944 cram suite docs and monopam Config.Paths alias docs.

--- test/rules/test_e943.mli
+++ test/rules/test_e943.mli
@@ -1,3 +1,4 @@
 (** {!Merlint.E943} cram-fixture suite. *)
 
 val suite : string * unit Alcotest.test_case list
+(** [suite] is the Alcotest suite for E943 cram fixtures. *)
--- test/rules/test_e944.mli
+++ test/rules/test_e944.mli
@@ -1,3 +1,4 @@
 (** {!Merlint.E944} cram-fixture suite. *)
 
 val suite : string * unit Alcotest.test_case list
+(** [suite] is the Alcotest suite for E944 cram fixtures. *)
